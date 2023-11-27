2961ac322e46a35690259d564b325af4220dabba
keran-w <wangkr1106@gmail.com>
2023-11-27 12:14:30 -0800

    Add docs for initialize api

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,18 +12,46 @@ router.get('/', (req, res) => {
 });
 
 /**
- * Get all necessary account data
- * @api {get} /account/:id/initialize
+ *  @swagger
+ *  /api/v1/account/{id}/initialize:
+ *    get:
+ *      description: Get all necessary account data
+ *      parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         type: string
+ *         description: Account ID
+ *         example: 109252111498807689
+ *      responses:
+ *          200:
+ *              description: A successful response
+ *              content:
+ *                application/json:
+ *                  schema:
+ *                    type: object
+ *                    properties:
+ *                      accountInfo:
+ *                        type: object
+ *                      accountFollowers:
+ *                        type: object
+ *                      accountFollowing:
+ *                        type: object
  */
 router.get('/account/:id/initialize', (req, res) => {
     const { id } = req.params;
     const accountInfo = getAccountInfoService(id);
-    const accountPosts = getAccountPostsService(id);
+    // const accountPosts = getAccountPostsService(id);
     const accountFollowers = getAccountFollowersService(id);
     const accountFollowing = getAccountFollowingService(id);
-    Promise.all([accountInfo, accountPosts, accountFollowers, accountFollowing])
+    Promise.all([accountInfo, accountFollowers, accountFollowing])
         .then(data => {
-            res.json(data)
+            res.json({
+                accountInfo: data[0],
+                // accountPosts: data[1],
+                accountFollowers: data[1],
+                accountFollowing: data[2],
+            });
         })
         .catch(error => {
             res.json(error);
