493fc5d5e79a9c49676286fb789fd11f51df2971
keran-w <wangkr1106@gmail.com>
2023-11-27 12:14:30 -0800

    Add docs for initialize api

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+import path from 'path';
 import 'module-alias/register.js';
 
 import express from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import routes from './src/routes/index.js';
 
 const app = express();
 const port = process.env.PORT || 10000;
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Mastodon Visualization Data API',
+            version: '1.0.0',
+            description: 'TODO: Add description',
+        },
+    },
+    apis: [path.resolve('./src/routes/*.js')],
+};
+const swaggerSpec = swaggerJSDoc(options);
 
 app.use('/api/v1', routes);
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const server = app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
