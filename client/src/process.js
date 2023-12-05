99d06995f85a2630e583d1e38335874175decea8
Kitwradr <suhas.2ab@gmail.com>
2023-12-04 17:02:36 -0800

    Fixed prettier issues

--- client/src/process.js
+++ client/src/process.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const baseURL = "http://localhost:10000";
+const baseURL = 'http://localhost:10000';
 
 /**
  * Function displays the overlay
@@ -40,24 +40,22 @@ export function hideSpinner() {
  * Updates the network graph with the user ID entered in the input box
  */
 
-export function updateID(){ 
-    console.log("Update ID called");
+export function updateID() {
+    console.log('Update ID called');
     showSpinner();
 
-    let userID = document.getElementById("idInput").value;
-
+    let userID = document.getElementById('idInput').value;
 
     if (!/^\d+$/.test(userID)) {
-        alert("Please enter a valid number.");
-        console.log("Invalid user ID entered");
+        alert('Please enter a valid number.');
+        console.log('Invalid user ID entered');
         hideSpinner();
         return;
     }
 
-    console.log("User ID", userID);
+    console.log('User ID', userID);
 
     let apiURL = `${baseURL}/api/v1/account/${userID}/initialize`;
-
 
     // Need to write a common http request function with error handling
     try {
@@ -67,115 +65,107 @@ export function updateID(){
                 try {
                     console.log(json);
                     const [nodes, edges] = processData(json, userID);
-                    console.log("Nodes and edges", nodes, edges);
+                    console.log('Nodes and edges', nodes, edges);
                     drawFromResponse(nodes, edges);
                     hideSpinner();
                 } catch (error) {
-                    console.log("Error in processing response", error);
+                    console.log('Error in processing response', error);
                     // Handle or rethrow the error as needed
                 }
             });
     } catch (error) {
-        alert("Error fetching data from API. Please try again later.");
-        console.log("Error", error);
+        alert('Error fetching data from API. Please try again later.');
+        console.log('Error', error);
         hideSpinner();
     }
 }
 
 /**
- * 
+ *
  * @param {JSON} json Response json from the API
  * @param {Number} mainID
  * @returns {Array} [nodes,edges]
- * 
+ *
  * Function takes in the JSON response from the API and the main user ID
  * Returns an array of nodes and edges to be used in the network graph
- * 
- * 
+ *
+ *
  */
 
-export function processData(json, mainID = '109252111498807689'){
-
+export function processData(json, mainID = '109252111498807689') {
     nodes_json = json.accountInfoList;
     edges_json = json.relations;
 
-    main_node = nodes_json.filter(node => node.id == mainID)[0];
+    main_node = nodes_json.filter((node) => node.id == mainID)[0];
 
     let nodes = [];
     let nodesCreated = new Set(); // to keep track of nodes created using the ids of accounts
-    nodes.push(
-        {
-            id: main_node.id,
-            value: 20,
-            label: main_node.display_name
-        }
-    );
+    nodes.push({
+        id: main_node.id,
+        value: 20,
+        label: main_node.display_name,
+    });
 
-    nodesCreated.add(main_node.id); 
+    nodesCreated.add(main_node.id);
 
     let edges = [];
 
-    nodes_json.forEach(node => {
+    nodes_json.forEach((node) => {
         if (node.id != main_node.id) {
-            nodes.push(
-                {
-                    id: node.id,
-                    value: 10,
-                    label: node.display_name
-                }
-            );
-            console.log("Added node", node.display_name);
-            nodesCreated.add(node.id); 
+            nodes.push({
+                id: node.id,
+                value: 10,
+                label: node.display_name,
+            });
+            console.log('Added node', node.display_name);
+            nodesCreated.add(node.id);
         }
     });
-    
-    for (let key in edges_json){
+
+    for (let key in edges_json) {
         let followers = edges_json[key];
-        followers.forEach(follower => {
-            edges.push(
-                {
-                    from: follower,
-                    to: key
-                }
-            );
+        followers.forEach((follower) => {
+            edges.push({
+                from: follower,
+                to: key,
+            });
         });
     }
 
-
-    return [nodes,edges];
+    return [nodes, edges];
 }
 
 /**
  * Draws the network graph using the nodes and edges array
- * @param {*} nodes  
- * @param {*} edges 
+ * @param {*} nodes
+ * @param {*} edges
  */
 
 export function drawFromResponse(nodes, edges) {
-    let container = document.getElementById("mynetwork");
+    let container = document.getElementById('mynetwork');
     let data = {
         nodes: nodes,
         edges: edges,
     };
     let options = {
         nodes: {
-            shape: "dot",
+            shape: 'dot',
             color: {
-                border: "#222222",
-                background: "#666666",
+                border: '#222222',
+                background: '#666666',
             },
-            font: { color: "#eeeeee" },
+            font: { color: '#eeeeee' },
         },
         edges: {
-            color: "lightgray",
+            color: 'lightgray',
             arrows: {
                 to: {
-                    enabled: true
-                }
+                    enabled: true,
+                },
             },
             smooth: {
                 enabled: false,
-                type: 'continuous'
+                type: 'continuous',
             },
             scaling: {
                 min: 1,
@@ -185,38 +175,38 @@ export function drawFromResponse(nodes, edges) {
                     min: 14,
                     max: 30,
                     maxVisible: 30,
-                    drawThreshold: 5
-                }
+                    drawThreshold: 5,
+                },
             },
-            length: 100
+            length: 100,
         },
         physics: {
             adaptiveTimestep: true,
             barnesHut: {
                 gravitationalConstant: -8000,
                 springConstant: 0.04,
-                springLength: 95
+                springLength: 95,
             },
             stabilization: {
-                iterations: 2000
-            }
+                iterations: 2000,
+            },
         },
         layout: {
             randomSeed: 191006,
-            improvedLayout: true
-        }
+            improvedLayout: true,
+        },
     };
     network = new vis.Network(container, data, options);
-    network.once("beforeDrawing", function () {
+    network.once('beforeDrawing', function () {
         network.focus(0, {
             scale: 12,
         });
     });
-    network.once("afterDrawing", function () {
+    network.once('afterDrawing', function () {
         network.fit({
             animation: {
                 duration: 1500,
-                easingFunction: "linear",
+                easingFunction: 'linear',
             },
         });
     });
--- client/src/process.test.js
+++ client/src/process.test.js
@@ -1,6 +1,10 @@
 // Import the functions to be tested
-import { showOverlay, hideOverlay, showSpinner, hideSpinner } from './process.js';
-
+import {
+    showOverlay,
+    hideOverlay,
+    showSpinner,
+    hideSpinner,
+} from './process.js';
 
 const { JSDOM } = require('jsdom');
 
@@ -9,34 +13,45 @@ const html = '<div id="overlay"></div><div id="loadingSpinner"></div>';
 const dom = new JSDOM(html);
 global.document = dom.window.document;
 
-
-
 describe('Overlay and Spinner Functions', () => {
-  beforeEach(() => {
-    // Reset the DOM before each test
-    dom.window.document.getElementById('overlay').style.display = 'none';
-    dom.window.document.getElementById('loadingSpinner').style.display = 'none';
-  });
+    beforeEach(() => {
+        // Reset the DOM before each test
+        dom.window.document.getElementById('overlay').style.display = 'none';
+        dom.window.document.getElementById('loadingSpinner').style.display =
+            'none';
+    });
 
-  test('showOverlay should set display to block', () => {
-    showOverlay();
-    expect(dom.window.document.getElementById('overlay').style.display).toBe('block');
-  });
+    test('showOverlay should set display to block', () => {
+        showOverlay();
+        expect(
+            dom.window.document.getElementById('overlay').style.display
+        ).toBe('block');
+    });
 
-  test('hideOverlay should set display to none', () => {
-    hideOverlay();
-    expect(dom.window.document.getElementById('overlay').style.display).toBe('none');
-  });
+    test('hideOverlay should set display to none', () => {
+        hideOverlay();
+        expect(
+            dom.window.document.getElementById('overlay').style.display
+        ).toBe('none');
+    });
 
-  test('showSpinner should set overlay and loadingSpinner display to block', () => {
-    showSpinner();
-    expect(dom.window.document.getElementById('overlay').style.display).toBe('block');
-    expect(dom.window.document.getElementById('loadingSpinner').style.display).toBe('block');
-  });
+    test('showSpinner should set overlay and loadingSpinner display to block', () => {
+        showSpinner();
+        expect(
+            dom.window.document.getElementById('overlay').style.display
+        ).toBe('block');
+        expect(
+            dom.window.document.getElementById('loadingSpinner').style.display
+        ).toBe('block');
+    });
 
-  test('hideSpinner should set overlay and loadingSpinner display to none', () => {
-    hideSpinner();
-    expect(dom.window.document.getElementById('overlay').style.display).toBe('none');
-    expect(dom.window.document.getElementById('loadingSpinner').style.display).toBe('none');
-  });
+    test('hideSpinner should set overlay and loadingSpinner display to none', () => {
+        hideSpinner();
+        expect(
+            dom.window.document.getElementById('overlay').style.display
+        ).toBe('none');
+        expect(
+            dom.window.document.getElementById('loadingSpinner').style.display
+        ).toBe('none');
+    });
 });
