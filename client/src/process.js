1d50d6483d2fd4a263eb99f1a28f15e0002e4196
Suhas <suhas.2ab@gmail.com>
2023-12-06 16:11:04 -0800

    Added client logic, integration, UI, validations (#8)
    Completes #30 
    
    Co-authored-by: Hao Luo <alex.haoluo@gmail.com>

---
+++ client/src/process.js
@@ -0,0 +1,223 @@
+/* eslint-disable no-undef */
+const baseURL = 'http://localhost:10000';
+
+/**
+ * Function displays the overlay
+ */
+
+export function showOverlay() {
+    document.getElementById('overlay').style.display = 'block';
+}
+
+/**
+ * Function hides the overlay
+ */
+
+export function hideOverlay() {
+    document.getElementById('overlay').style.display = 'none';
+}
+
+/**
+ * Function displays the loading spinner and overlay
+ */
+
+export function showSpinner() {
+    showOverlay();
+    document.getElementById('loadingSpinner').style.display = 'block';
+}
+
+/**
+ * Function hides the loading spinner and overlay
+ */
+
+export function hideSpinner() {
+    hideOverlay();
+    document.getElementById('loadingSpinner').style.display = 'none';
+}
+
+/**
+ * Funntion to validate the user ID entered in the input box
+ * @param {Number} userID user ID entered in the input box
+ * @returns {Boolean} true if the user ID is valid, else false
+ */
+export function verifyUserID(userID) {
+    if (!/^\d+$/.test(userID)) {
+        console.log('Invalid user ID entered');
+        return false;
+    }
+
+    console.log('User ID', userID);
+
+    return true;
+}
+
+/**
+ * Function is called when the user clicks on the "Update" button
+ * Updates the network graph with the user ID entered in the input box
+ */
+
+export function updateID() {
+    console.log('Update ID called');
+    showSpinner();
+
+    let userID = document.getElementById('idInput').value;
+
+    if (!verifyUserID(userID)) {
+        alert('Please enter a valid number.');
+        hideSpinner();
+        return;
+    }
+
+    let apiURL = `${baseURL}/api/v1/account/${userID}/initialize`;
+
+    // Need to write a common http request function with error handling
+    fetch(apiURL)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            const [nodes, edges] = processData(json, userID);
+            console.log('Nodes and edges', nodes, edges);
+            drawFromResponse(nodes, edges);
+            hideSpinner();
+        })
+        .catch((error) => {
+            console.log(
+                'Error fetching data from API. Please try again later.'
+            );
+            alert('Error fetching data from API. Please try again later.');
+            console.log('Error', error);
+            hideSpinner();
+        });
+}
+
+/**
+ *
+ * @param {JSON} json Response json from the API
+ * @param {Number} mainID
+ * @returns {Array} [nodes,edges]
+ *
+ * Function takes in the JSON response from the API and the main user ID
+ * Returns an array of nodes and edges to be used in the network graph
+ *
+ *
+ */
+
+export function processData(json, mainID) {
+    nodes_json = json.accountInfoList;
+    edges_json = json.relations;
+
+    main_node = nodes_json.filter((node) => node.id == mainID)[0];
+
+    let nodes = [];
+    let nodesCreated = new Set(); // to keep track of nodes created using the ids of accounts
+    nodes.push({
+        id: main_node.id,
+        value: 20, // size of the node in the graph
+        label: main_node.display_name,
+    });
+
+    nodesCreated.add(main_node.id);
+
+    let edges = [];
+
+    nodes_json.forEach((node) => {
+        if (node.id != main_node.id) {
+            nodes.push({
+                id: node.id,
+                value: 10, // lower size for followers
+                label: node.display_name,
+            });
+            console.log('Added node', node.display_name);
+            nodesCreated.add(node.id);
+        }
+    });
+
+    for (let key in edges_json) {
+        let followers = edges_json[key];
+        followers.forEach((follower) => {
+            edges.push({
+                from: follower,
+                to: key,
+            });
+        });
+    }
+
+    return [nodes, edges];
+}
+
+/**
+ * Draws the network graph using the nodes and edges array
+ * @param {*} nodes
+ * @param {*} edges
+ */
+
+export function drawFromResponse(nodes, edges) {
+    let container = document.getElementById('mynetwork');
+    let data = {
+        nodes: nodes,
+        edges: edges,
+    };
+    let options = {
+        nodes: {
+            shape: 'dot',
+            color: {
+                border: '#222222',
+                background: '#666666',
+            },
+            font: { color: '#eeeeee' },
+        },
+        edges: {
+            color: 'lightgray',
+            arrows: {
+                to: {
+                    enabled: true,
+                },
+            },
+            smooth: {
+                enabled: false,
+                type: 'continuous',
+            },
+            scaling: {
+                min: 1,
+                max: 3,
+                label: {
+                    enabled: true,
+                    min: 14,
+                    max: 30,
+                    maxVisible: 30,
+                    drawThreshold: 5,
+                },
+            },
+            length: 100,
+        },
+        physics: {
+            adaptiveTimestep: true,
+            barnesHut: {
+                gravitationalConstant: -8000,
+                springConstant: 0.04,
+                springLength: 95,
+            },
+            stabilization: {
+                iterations: 2000,
+            },
+        },
+        layout: {
+            randomSeed: 191006,
+            improvedLayout: true,
+        },
+    };
+    network = new vis.Network(container, data, options);
+    network.once('beforeDrawing', function () {
+        network.focus(0, {
+            scale: 12,
+        });
+    });
+    network.once('afterDrawing', function () {
+        network.fit({
+            animation: {
+                duration: 1500,
+                easingFunction: 'linear',
+            },
+        });
+    });
+}
