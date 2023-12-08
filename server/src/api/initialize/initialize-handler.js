0d9c22ec2ed4441d044e94e4fd4bc8329531fa63
Keran Wang <83668340+keran-w@users.noreply.github.com>
2023-12-08 12:25:02 -0800

    Initialize API for Basic Network Visualization (#32)
    Completes #6

---
+++ server/src/api/initialize/initialize-handler.js
@@ -0,0 +1,53 @@
+import { accountInitializeService } from '../../service/initialize/initialize-service.js';
+import FediverseAPIFactory from '../../fediverse/fediverse-api-factory.js';
+
+import GUN from 'gun';
+import DAO from '../../db/dao.js';
+import GunDBAdaptor from '../../db/gun/gun-db-adapator.js';
+
+// Create database and API instances
+const db = new DAO(new GunDBAdaptor(GUN()));
+
+/**
+ * Asynchronous route handler for retrieving initialization data of a user account in the Fediverse network.
+ * It parses the account URL from the query parameters, validates it, and then uses the Fediverse API to get
+ * the account details with its following and followers.
+ * If the account is found, it returns the graph data (following relation list and account info list);
+ * otherwise, it sends appropriate error responses.
+ *
+ * @param {object} req - The Express request object, containing the path parameter 'id' representing the account id.
+ * @param {object} res - The Express response object, used to send back the initialize result or error messages.
+ * @returns {Promise<void>} - A promise that resolves when the response is sent.
+ */
+export default async function accountInitializeHandler(req, res) {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({
+            error: 'Account ID is required as a path parameter.',
+        });
+    }
+
+    try {
+        // Server is unknown given the id, create an adapter with default server
+        const api = FediverseAPIFactory.createAdapter();
+        const accountInitializeData = await accountInitializeService(
+            api,
+            id,
+            db
+        );
+
+        if (accountInitializeData.accountInfoList.length > 0) {
+            res.status(200).json(accountInitializeData);
+        } else {
+            res.status(404).json({
+                error: `Record not found for id: ${id}`,
+            });
+        }
+    } catch (error) {
+        console.error('Unexpected error during account initialize:', error);
+        res.status(500).json({
+            error: `Internal Server Error: ${error?.message}`,
+        });
+    }
+}
--- server/src/api/router.js
+++ server/src/api/router.js
@@ -1,7 +1,62 @@
 import express from 'express';
 import accountLookupHandler from './lookup/lookup-handler.js';
+import accountInitializeHandler from './initialize/initialize-handler.js';
 
 const router = express.Router();
+
+/**
+ *  @swagger
+ *  /api/v1/account/{id}/initialize:
+ *      get:
+ *        description: Get all necessary account data for a specified account.
+ *        parameters:
+ *          - in: path
+ *            name: id
+ *            required: true
+ *            type: string
+ *            description: The unique identifier of the account.
+ *            example: "109252111498807689"
+ *        responses:
+ *          200:
+ *            description: A successful response containing account data.
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: object
+ *                  properties:
+ *                    relations:
+ *                      type: object
+ *                      additionalProperties:
+ *                        type: array
+ *                        items:
+ *                          type: string
+ *                      description: A map of account relationships, showing connections between various accounts.
+ *                    accountInfoList:
+ *                      type: array
+ *                      items:
+ *                        type: object
+ *                        properties:
+ *                          id:
+ *                            type: string
+ *                            description: Unique identifier of the account.
+ *                          username:
+ *                            type: string
+ *                            description: Username of the account.
+ *                          display_name:
+ *                            type: string
+ *                            description: Display name of the account.
+ *                          following_count:
+ *                            type: integer
+ *                            description: Number of accounts this account is following.
+ *                          followers_count:
+ *                            type: integer
+ *                            description: Number of followers this account has.
+ *                          statuses_count:
+ *                            type: integer
+ *                            description: Number of statuses posted by this account.
+ *                      description: A list containing information for each account.
+ */
+router.get('/account/:id/initialize', accountInitializeHandler);
 
 /**
  *  @swagger
