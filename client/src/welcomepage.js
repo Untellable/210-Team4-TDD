5578993d229b5ec8b6c057ff0e13f33bc0583baa
yunlongwangsd <yuw151@ucsd.edu>
2023-12-08 19:52:00 -0800

    expandable views on welcome page instructions & links

--- client/src/welcomepage.js
+++ client/src/welcomepage.js
@@ -1,10 +1,21 @@
-const hoverLink = document.querySelector('.hover-link');
-const hoverImage = document.querySelector('.hover-image');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const expandableContainers = document.querySelectorAll('.expandable-container');
 
-hoverLink.addEventListener('mouseover', () => {
-    hoverImage.classList.add('visible'); // Add the 'visible' class for image transition animation
-});
+    expandableContainers.forEach(container => {
+        const expandableTrigger = container.querySelector('.expandable-trigger');
+        const expandableContent = container.querySelector('.expandable-content');
+        
+        expandableTrigger.addEventListener('click', () => {
+            // Toggle the content visibility
+            const isVisible = expandableContent.style.display === "block";
+            expandableContent.style.display = isVisible ? "none" : "block";
 
-hoverLink.addEventListener('mouseout', () => {
-    hoverImage.classList.remove('visible');
+            // Toggle the arrow direction
+            expandableTrigger.classList.toggle('active', !isVisible);
+
+            // Toggle the background color of the expandable container and trigger
+            container.style.backgroundColor = isVisible ? 'transparent' : 'rgb(19, 20, 25)';
+            expandableTrigger.style.backgroundColor = isVisible ? 'transparent' : 'rgb(19, 20, 25)';
+        });
+    });
 });
