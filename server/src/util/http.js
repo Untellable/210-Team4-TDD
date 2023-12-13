68c8af6ae0204c00f3b2dad8af2b814d6411423c
keran-w <wangkr1106@gmail.com>
2023-12-13 01:42:12 -0800

    Add unit test for timeout and retry count

--- server/src/util/http.js
+++ server/src/util/http.js
@@ -8,16 +8,38 @@ const httpInstance = axios.create({
 // retry requests whe failed
 axiosRetry(httpInstance, {
     retries: 3, // Number of retries
+    /*    retryCondition(error) {
+            // Conditional check the error status code
+            console.log(`http error: ${error}`);
+            console.log(`http error.code: ${error.code}`)
+            console.log(`retryCondition: ${error.response.status}`);
+            switch (error.response.status) {
+                case 404:
+                case 429:
+                    console.log(`retrying request`);
+                    return true; // Retry request with response status code 404 or 429
+                default:
+                    return false; // Do not retry the others
+            }
+        },*/
+
     retryCondition(error) {
-        // Conditional check the error status code
-        switch (error.response.status) {
-            case 404:
-            case 429:
-                console.log(`retrying request`);
-                return true; // Retry request with response status code 404 or 429
-            default:
-                return false; // Do not retry the others
+        // Check for timeout error
+        if (error.code === 'ECONNABORTED') {
+            console.log('retrying request due to timeout');
+            return true;
         }
+
+        // Ensure error.response exists before accessing status
+        if (
+            error.response &&
+            (error.response.status === 404 || error.response.status === 429)
+        ) {
+            console.log('retrying request');
+            return true;
+        }
+
+        return false;
     },
     retryDelay: axiosRetry.exponentialDelay, // Exponential back-off retry delay between requests
     shouldResetTimeout: true,
