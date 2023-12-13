e2a29bc7ff0e321c64be78d8e5a0b444f8f36df8
yunlongwangsd <yuw151@ucsd.edu>
2023-12-13 09:48:55 -0800

    remove comment in client.js

--- client/client.js
+++ client/client.js
@@ -5,7 +5,6 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'src')));
 app.get('/', (req, res) => {
-    // res.sendFile(path.join(__dirname, 'src', 'visjs.html'));
     res.sendFile(path.join(__dirname, 'src', 'welcomepage.html'));
     
 });
