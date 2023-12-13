26c69ca33e1dc4a81e793767839a1767f5815d50
Hao Luo <alex.haoluo@gmail.com>
2023-12-13 07:50:32 -0800

    remove comment and set timeout to 1000ms

--- server/src/util/http.js
+++ server/src/util/http.js
@@ -2,27 +2,12 @@ import axios from 'axios';
 import axiosRetry from 'axios-retry';
 
 const httpInstance = axios.create({
-    timeout: 5000,
+    timeout: 1000,
 });
 
 // retry requests whe failed
 axiosRetry(httpInstance, {
     retries: 3, // Number of retries
-    /*    retryCondition(error) {
-            // Conditional check the error status code
-            console.log(`http error: ${error}`);
-            console.log(`http error.code: ${error.code}`)
-            console.log(`retryCondition: ${error.response.status}`);
-            switch (error.response.status) {
-                case 404:
-                case 429:
-                    console.log(`retrying request`);
-                    return true; // Retry request with response status code 404 or 429
-                default:
-                    return false; // Do not retry the others
-            }
-        },*/
-
     retryCondition(error) {
         // Check for timeout error
         if (error.code === 'ECONNABORTED') {
